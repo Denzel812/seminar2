0889844c5f5014ecf99863e9fe0af75d82d2e5af
Denis <denis-gorbunov-89@mail.ru>
2022-11-25 07:40:14 +0300

    realized  tasc17

--- tasc17/Program.cs
+++ tasc17/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //onsole.WriteLine("Hello, World!");
+Console.WriteLine("Введите коорденаты по точке X");
+int X= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите кординаты по точке Y");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X>0 && Y>0)
+{
+    Console.WriteLine("Точка находиться в первой координатной четверти");
+}
+else if (X>0 && Y<0)
+{
+    Console.WriteLine("Точка находиться во второй координатной чертвити");
+}
+else if (X<0 && Y<0)
+{
+    Console.WriteLine("Точка находиться в третий координатной четверти");
+}
+else if (X<0 && Y>0)
+{
+    Console.WriteLine("Точка находиться в четвёртой координатной четверти");
+}
+else
+{
+    Console.WriteLine("Такой координатной четверти не существует");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
