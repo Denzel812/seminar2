7219d1ac9589e5197505b9931c4532f8e5051e32
Denis <denis-gorbunov-89@mail.ru>
2022-11-26 09:38:32 +0300

    realized task18

---
+++ task18/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите номер четверти");
+int duarter = Convert.ToInt32(Console.ReadLine());
+
+if (duarter==1){
+    Console.WriteLine("X>0 & Y>0");
+}
+else if (duarter==2){
+    Console.WriteLine("X>0 &  Y<0");
+}
+else if (duarter==3){
+    Console.WriteLine("X<0 & Y<0");
+}
+else if (duarter==4){
+    Console.WriteLine("X<0 & Y>0");
+}
+else
+{
+    Console.WriteLine("Такой четверти не существует");
+}
+
+    
