c6e8080ea109da9da51cdf0ee99ce9d5c6167fe0
Denis <denis-gorbunov-89@mail.ru>
2022-12-01 07:35:25 +0300

    realized task22

---
+++ task22/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<=N; i++)
+{
+    Console.Write(i*i + " ");
+}
